3aa5c6f11cd6c9f72f8d872b67c27cadbe3f7e75
ghazli lydia widad <167114926+llww2020@users.noreply.github.com>
2024-11-20 16:32:41 +0100

    coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,24 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+float sizeimage = w * h * 8 ; 
+   
+   // Convert based on the unit
+   if (unit == "bt") {
+       return (sizeimage ) ; // Size in bits
+   } else if (unit == "ko") {
+      sizeimage = sizeimage /1024.0 ;
+         return (sizeimage ); // Size in kilobits
+   } else if (unit == "mo") {
+      sizeimage  = sizeimage / (1024.0 * 1024.0);
+       return(sizeimage); // Size in megabits
+   } else if (unit == "go") {
+      sizeimage = sizeimage / (1024.0 * 1024.0 * 1024.0);
+       return (sizeimage); // Size in gigabits
+   } else {
+       // Invalid unit, return 0
+       return 0;
+       }
    // YOUR CODE HERE - END
    return 0;
 }
