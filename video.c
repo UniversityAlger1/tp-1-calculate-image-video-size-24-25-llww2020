fe3774b865f7863a020903abbdde4ef7a89089a5
ghazli lydia widad <167114926+llww2020@users.noreply.github.com>
2024-11-21 18:44:26 +0100

    video.c

--- video.c
+++ video.c
@@ -10,6 +10,34 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+ // Taille par image pour chaque type (en bits)
+    float sizeColoredFrame = w * h * 24 ;  // Couleur (24 bits par pixel)
+    float sizegreyFrame = w * h * 8 ;        // Noir et blanc (8 bits par pixel)
+
+    // Nombre total d'images pour chaque section
+    int totalColoredFrames = fps * durationMovie; 
+    int totalgreyFrames = fps * durationCredits;
+
+    // Taille totale de chaque section (en bits)
+    float totalColoredSize = sizeColoredFrame * totalColoredFrames;
+    float totalgreySize = sizegreyFrame * totalgreyFrames;
+
+    // Taille totale de la vidéo (en bits)
+    float SizeVideo = totalColoredSize + totalgreySize;
+
+    // Conversion selon l'unité spécifiée
+    if (strcmp(unit, "bt") == 0) {
+        return SizeVideo; // Taille en bits
+    } else if (strcmp(unit, "ko") == 0) {
+        return SizeVideo / 1024.0; // Taille en kilobits
+    } else if (strcmp(unit, "mo") == 0) {
+        return SizeVideo / (1024.0 * 1024.0); // Taille en mégabits
+    } else if (strcmp(unit, "go") == 0) {
+        return SizeVideo / (1024.0 * 1024.0 * 1024.0); // Taille en gigabits
+    } else {
+        // Unité invalide
+        return 0;
+    }
 
    // YOUR CODE HERE - END
    return 0;
